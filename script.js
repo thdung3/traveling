d3e4bc52fb89bba09effb6e6ad542cb9f6ff6275
jeesun <luckymeday@gmail.com>
2020-07-03 14:28:40 +0700

    community

---
+++ script.js
@@ -0,0 +1,25 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 300;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        console.log('counter.innerText:',counter.innerText)
+        console.log('target:',target)
+
+        const inc = target / speed;
+        console.log('inc:',inc)
+
+        if(count < target) {
+            // counter.innerText = Math.ceil(count + inc);
+            counter.innerText = parseFloat((count + inc).toFixed(3));
+            console.log('counter:',Math.ceil(count + inc))
+            setTimeout(updateCount, 1);
+        }
+        else {
+            count.innerText = target;
+        }    
+}
+updateCount();
+});
