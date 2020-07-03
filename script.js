9d04604d3a226a91f35884235f3a227c83b9622d
jeesun <luckymeday@gmail.com>
2020-07-03 15:53:33 +0700

    first commit

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
-const bgDark = document.querySelector('.bg-dark');
-window.onscroll = function () {
-    var top = window.scrollY;
-    console.log(top);
-    if (top > 10) {
-        bgDark.classList.add('scrolled')
-    } else {
-        bgDark.classList.remove('scrolled')
-    }
-}
-var TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 1000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-    if (this.isDeleting) { delta /= 2; }
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-    setTimeout(function () {
-        that.tick();
-    }, delta);
-};
+const counters = document.querySelectorAll('.counter');
+const speed = 300;
 
-window.onload = function () {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        console.log('counter.innerText:',counter.innerText)
+        console.log('target:',target)
+
+        const inc = target / speed;
+        console.log('inc:',inc)
+
+        if(count < target) {
+            // counter.innerText = Math.ceil(count + inc);
+            counter.innerText = parseFloat((count + inc).toFixed(3));
+            console.log('counter:',Math.ceil(count + inc))
+            setTimeout(updateCount, 1);
         }
-    }
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
+        else {
+            count.innerText = target;
+        }    
+}
+updateCount();
+});
